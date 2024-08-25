cbcd1160dbafc722f3bfff6c059ae173a59be473
vinhtran113 <tvih6693@gmail.com>
2024-08-25 10:29:39 +0700

    up
    upp

---
+++ baitap01/App.js
@@ -0,0 +1,25 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import IntroScreen from './IntroScreen';
+import HomeScreen from './HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Intro">
+        <Stack.Screen
+          name="Intro"
+          component={IntroScreen}
+          options={{ headerShown: false }} // Ẩn header cho trang giới thiệu
+        />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Trang Chủ' }} // Hiển thị tiêu đề cho trang chủ
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
